90a8604ab42c5cf0b82f5838be9a7a2e1a8ecb70
Willow-tech <66530245+Willow-tech@users.noreply.github.com>
2021-10-06 12:04:48 +0200

    correction

--- gsbMVC/vues/v_sommaire_comptable.php
+++ gsbMVC/vues/v_sommaire_comptable.php
@@ -13,10 +13,10 @@
 				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
 			</li>
            <li class="smenu">
-              <a href="index.php?uc=validerFrais&action=" title="Valider fiche de frais ">Valider fiche de frais</a>
+              <a href="index.php?uc=validerFrais&action=selectionnerVisiteur" title="Valider fiche de frais ">Valider fiche de frais</a>
            </li>
            <li class="smenu">
-              <a href="index.php?uc=paiementFrais&action=" title="Suivre paiement">Suivre paiement</a>
+              <a href="index.php?uc=paiementFrais&action=voirFicheFrais" title="Suivre paiement">Suivre paiement</a>
            </li>
  	   <li class="smenu">
               <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
