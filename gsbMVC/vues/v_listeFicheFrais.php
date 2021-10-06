0a0e82d60f04b770d7e5433ad19c5d33f14bdc2b
NegiAdd <85967440+NegiAdd@users.noreply.github.com>
2021-10-06 11:27:29 +0200

    v_listeFicheFrais

---
+++ gsbMVC/vues/v_listeFicheFrais.php
@@ -0,0 +1,32 @@
+<div id="contenu">
+    <h2>Suivi du paiement</h2>
+    <h3>Fiche frais &agrave; selectionner : </h3>
+    <form action="index.php?uc=suiviPaiement&action=voirFicheFrais" method="post">
+        <div class="corpsForm">
+
+            <p>
+
+                <label for="listeFicheFrais" accesskey="n">Fiche :</label>
+                <select id="listeFicheFrais" name="listeFicheFrais">
+                    <?php
+                    foreach ($lesFichesFrais as $uneFicheFrais) {
+                        $id = $uneFicheFrais['id'];
+                        $nom = $uneFicheFrais['nom'];
+                        $prenom = $uneFicheFrais['prenom'];
+                        $mois = $uneFicheFrais['mois'];
+                        ?>
+                        <option value="<?php echo $id . "/" . $mois; ?>"><?php echo $prenom . " " . $nom . " " . $mois ?></option>
+                        <?php
+                    }
+                    ?>
+
+                </select>
+            </p>
+        </div>
+        <div class="piedForm">
+            <p>
+                <input id="ok" type="submit" value="Valider" size="20" />
+                <input id="annuler" type="reset" value="Effacer" size="20" />
+            </p>
+        </div>
+    </form>
